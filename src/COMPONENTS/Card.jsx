17deff32b8257aab7d4e5024fb87898ae630916a
Vianney7000 <vianneyobodozie8@gmail.com>
2026-02-20 22:01:06 -0800

    21st feb

--- src/COMPONENTS/Card.jsx
+++ src/COMPONENTS/Card.jsx
@@ -1,36 +1,34 @@
 import { Button } from "@mui/material"
-import { FiArrowLeft, FiArrowRight } from "react-icons/fi"
+import { FiArrowRight } from "react-icons/fi"
 import jeanmodel from '../assets/IMAGES/girlonjeancloth.webp'
-import malemodel2 from '../assets/IMAGES/malemodel2.jpg'
-import { motion } from 'framer-motion'
+import heroimg2 from '../assets/IMAGEs/fashionimage3.avif'
 import './Card.css'
 
 
 const Card = () => {
   return (
     <div className='card'>
-        <div className="container-1">
-                <div className="write-up">
-                    Quality jean jaket designed to sute your style <Button> <FiArrowRight/> </Button>
-               </div>
 
-                <div className="img-1">
-                    <img src={jeanmodel} alt="" />
-               </div>
-            </div>
+      <div className="container-1">
+         <div className="write-up">
+            Quality jean jaket designed to sute your style <Button> <FiArrowRight/> </Button>
+         </div>
+
+        <div className="img-1">
+          <img src={jeanmodel} alt="" />
+        </div>
+      </div>
+
+      <div className="hero_image_display2">
+        <img src={heroimg2} alt="" />
+      </div>
+
+      <div className="image-text2">
+          <h1>Style Your Dream</h1>
+      </div>
 
 
-              <div className="container-1">
-                
-                <div className="img-1">
-                    <img src={malemodel2} alt="" />
-               </div>
-
-                 <div className="write-up">
-                    Highlighting Quality and Confidence <Button> <FiArrowLeft/> </Button>
-               </div>
-
-            </div>
+             
     </div>
   )
 }
